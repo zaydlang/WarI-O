e1aa4c3c59479aac4cc60f07fc7a02214f4551a4
ZaydQumsieh <zayd@qumsieh.net>
2018-06-20 14:53:50 -0700

    attempted an ai using sigmoid, some bugs

--- WarIO.lua
+++ WarIO.lua
@@ -19,7 +19,7 @@ function getBlocks(blocks)
   for i = 0, 16 do
     blocks[i] = {}
     for j = 0, 13 do -- 16 * 13 = 208
-      local block = {}
+      blocks[i][j] = {}
       local TempX = PlayerX + (i) * 16
       local BlockX = math.floor((TempX % 256) / 16)
       local BlockY = j * 16
@@ -27,10 +27,8 @@ function getBlocks(blocks)
       local BlockAddress = 0
       if (math.floor(TempX / 256) % 2 == 1) then BlockAddress = 208 end
       BlockAddress = 0x0500 + BlockAddress + BlockY + BlockX
-      if (memory.readbyte(BlockAddress) ~= 0 and BlockY < 13 * 16 and BlockY >= 0) then block.value = 1 
-      else block.value = 0 end
-      
-      blocks[i][j] = block
+      if (memory.readbyte(BlockAddress) ~= 0 and BlockY < 13 * 16 and BlockY >= 0) then blocks[i][j].value = 1 
+      else blocks[i][j].value = 0 end
     end
   end
   
@@ -47,14 +45,27 @@ function getEnemies(blocks)
         enemy.x = math.floor(enemy.x % 256 / 16)
         enemy.y = math.floor((memory.readbyte(0x00CF + i) - 8) / 16)
         gui.text(0, i * 15 + 25, "Enemy coords: " .. enemy.x .. " " .. enemy.y)
-        enemy.value = 2
-        blocks[enemy.x + 1][enemy.y - 1] = enemy
+        blocks[enemy.x + 1][enemy.y - 1].value = 2
       end
     end
   end
   
-  blocks.enemies = enemies
   return blocks
+end
+
+function displayJoypad(joypad)
+  if (joypad["P1 A"] == true) then gui.drawBox(240, 40, 250, 55, "white", "white") end
+  gui.drawText(240, 40, "A")
+  if (joypad["P1 B"] == true) then gui.drawBox(240, 55, 250, 70, "white", "white") end
+  gui.drawText(240, 55, "B")
+  if (joypad["P1 Up"] == true) then gui.drawBox(240, 70, 250, 85, "white", "white") end
+  gui.drawText(240, 70, "U")
+  if (joypad["P1 Down"] == true) then gui.drawBox(240, 85, 250, 100, "white", "white") end
+  gui.drawText(240, 85, "D")
+  if (joypad["P1 Left"] == true) then gui.drawBox(240, 100, 250, 115, "white", "white") end
+  gui.drawText(240, 100, "L")
+  if (joypad["P1 Right"] == true) then gui.drawBox(240, 115, 250, 129, "white", "white") end
+  gui.drawText(240, 115, "R")
 end
 
 function display(blocks)
@@ -70,6 +81,81 @@ function display(blocks)
   end
 end
 
+function sigmoid(x)
+  return (1 / (1 + e^(-x)))
+end
+
+function firstLayer(specie)
+  local blocks = specie.blocks
+  
+  for i = 0, 16 do
+    for j = 0, 13 do
+      if (blocks[i][j].value ~= nil) then print("Wowzers! " .. i .. " " .. j) 
+        blocks[i][j].sig = sigmoid(blocks[i][j].value * blocks[i][j].weightA + blocks[i][j].weightB)
+      end
+    end
+  end
+  
+  return specie
+end
+
+function secondLayer(specie)
+  local nodes = specie.buttons
+  for i = 0, 6 do
+    local previous = nodes.previous
+    
+    local average = 0
+    for j, block in ipairs(previous) do
+      average = average + specie.blocks[block[0]][block[1]].sig
+    end
+    average = average / #previous
+    
+    if (average > 0.5) then nodes[i].pressed = true else nodes[i].pressed = false end
+  end
+  
+  return specie
+end
+
+function sendInput(input)
+  local buttons
+  buttons["P1 A"] = input[0].pressed
+  buttons["P1 B"] = input[1].pressed
+  buttons["P1 Down"] = input[2].pressed
+  buttons["P1 Left"] = input[3].pressed
+  buttons["P1 Right"] = input[4].pressed
+  buttons["P1 Up"] = input[5].pressed
+  
+  buttons["P1 Select"] = false
+  buttons["P1 Start"] = false
+  
+  joypad.set(buttons)
+end
+
+function getRandomSpecie() 
+  local specie = {}
+  local blocks = {}
+  specie.buttons = {}
+  specie.buttons.previous = {}
+  
+  for i = 0, 16 do
+    blocks[i] = {}
+    for j = 0, 13 do
+      local block = {}
+      block.weightA = math.random()
+      block.weightB = math.random()
+      blocks[i][j] = blocks
+      specie.buttons.previous[math.floor(math.random(6))][#specie.buttons.previous[math.floor(math.random(6))]][0] = i;
+      specie.buttons.previous[math.floor(math.random(6))][#specie.buttons.previous[math.floor(math.random(6))]][1] = j;
+    end
+  end
+  
+  specie.blocks = blocks
+  return specie
+end
+
+  
+local specie = getRandomSpecie()
+  
 while true do
   memory.writebyte(0x075A, 2)
   memory.writebyte(0x0787, 0x02)
@@ -79,6 +165,11 @@ while true do
   blocks = getBlocks(blocks)
   blocks = getEnemies(blocks)
   display(blocks)
+  displayJoypad(joypad.getimmediate())
+  
+  specie = firstLayer(specie)
+  specie = secondLayer(specie)
+  sendInput(specie.buttons)
   
   gui.text(0, 10, "Player Position: " .. PlayerX .. " " .. PlayerY)
   emu.frameadvance()
