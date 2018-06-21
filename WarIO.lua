5211d16e263b6fde65a69c0f21002d15e9001bdb
ZaydQumsieh <zayd@qumsieh.net>
2018-06-21 08:32:24 -0700

    generates random AI, needs to evolve.

--- WarIO.lua
+++ WarIO.lua
@@ -19,7 +19,7 @@ function getBlocks(blocks)
   for i = 0, 16 do
     blocks[i] = {}
     for j = 0, 13 do -- 16 * 13 = 208
-      blocks[i][j] = {}
+      local block = {}
       local TempX = PlayerX + (i) * 16
       local BlockX = math.floor((TempX % 256) / 16)
       local BlockY = j * 16
@@ -27,8 +27,10 @@ function getBlocks(blocks)
       local BlockAddress = 0
       if (math.floor(TempX / 256) % 2 == 1) then BlockAddress = 208 end
       BlockAddress = 0x0500 + BlockAddress + BlockY + BlockX
-      if (memory.readbyte(BlockAddress) ~= 0 and BlockY < 13 * 16 and BlockY >= 0) then blocks[i][j].value = 1 
-      else blocks[i][j].value = 0 end
+      if (memory.readbyte(BlockAddress) ~= 0 and BlockY < 13 * 16 and BlockY >= 0) then block.value = 10
+      else block.value = 0 end
+      
+      blocks[i][j] = block
     end
   end
   
@@ -45,12 +47,101 @@ function getEnemies(blocks)
         enemy.x = math.floor(enemy.x % 256 / 16)
         enemy.y = math.floor((memory.readbyte(0x00CF + i) - 8) / 16)
         gui.text(0, i * 15 + 25, "Enemy coords: " .. enemy.x .. " " .. enemy.y)
-        blocks[enemy.x + 1][enemy.y - 1].value = 2
+        enemy.value = -10
+        blocks[enemy.x + 1][enemy.y - 1] = enemy
       end
     end
   end
   
+  blocks.enemies = enemies
   return blocks
+end
+
+function getRandomGenome(number)
+  local genome = {}
+  
+  for i = 0, number do
+    local specie = {}
+    
+    -- Creating Layer 1
+    local NodesOne = {}
+    for j = 0, 16 do
+      NodesOne[j] = {}
+      for k = 0, 13 do
+        NodesOne[j][k] = {}
+        NodesOne[j][k].WeightA = math.random() - 0.5
+        NodesOne[j][k].WeightB = math.random() * 0.1
+        NodesOne[j][k].out = math.floor(math.random(6))
+        NodesOne[j][k].value = 0
+      end
+    end
+    specie.LayerOne = NodesOne
+    
+    -- Creating Layer 2
+    local NodesTwo = {}
+    for i = 0, 6 do
+      NodesTwo[i] = {}
+      NodesTwo[i].value = 0
+      NodesTwo[i].NumberToAverage = 0
+    end
+    specie.LayerTwo = NodesTwo
+    
+    genome[i] = specie
+  end
+  
+  return genome
+end
+
+function testSpecie(specie, blocks)
+  -- First Layer
+  local NodesOne = specie.LayerOne
+  for i = 0, 16 do
+    for j = 0, 13 do
+      NodesOne[i][j].value = sigmoid(NodesOne[i][j].WeightA * blocks[i][j].value + NodesOne[i][j].WeightB)
+    end
+  end
+  
+  -- Second Layer
+  local NodesTwo = specie.LayerTwo
+  for i = 0, 16 do
+    for j = 0, 13 do
+      NodesTwo[NodesOne[i][j].out].value = NodesTwo[NodesOne[i][j].out].value + NodesOne[i][j].value
+      NodesTwo[NodesOne[i][j].out].NumberToAverage = NodesTwo[NodesOne[i][j].out].NumberToAverage + 1
+    end
+  end
+  for i = 0, 6 do
+    NodesTwo[i].value = NodesTwo[i].value / NodesTwo[i].NumberToAverage
+    NodesTwo[i].NumberToAverage = 0
+  end
+  
+  -- Sending Input
+  local buttons = {}
+  buttons["P1 A"]      = NodesTwo[1].value > 0.5
+  buttons["P1 B"]      = NodesTwo[2].value > 0.5
+  buttons["P1 Up"]     = NodesTwo[3].value > 0.5
+  buttons["P1 Down"]   = NodesTwo[4].value > 0.5
+  buttons["P1 Left"]   = NodesTwo[5].value > 0.5
+  buttons["P1 Right"]  = NodesTwo[6].value > 0.5
+  buttons["P1 Start"]  = false
+  buttons["P1 Select"] = false
+  joypad.set(buttons)
+end
+
+function sigmoid(x) 
+  return 1 / (1 + math.exp(-1 * x))
+end
+
+function display(blocks)
+  for i = 0, 16 do
+    for j = 0, 13 do
+      if (blocks[i][j].value == 10) then
+        gui.drawBox(i * 2 * BoxRadius - BoxRadius, j * 2 * BoxRadius - BoxRadius + 32, i * 2 * BoxRadius + BoxRadius, j * 2 * BoxRadius + BoxRadius + 32, "white") 
+      end
+      if (blocks[i][j].value == -10) then
+        gui.drawBox(i * 2 * BoxRadius - BoxRadius, j * 2 * BoxRadius - BoxRadius + 32, i * 2 * BoxRadius + BoxRadius, j * 2 * BoxRadius + BoxRadius + 32, "red")
+      end
+    end
+  end
 end
 
 function displayJoypad(joypad)
@@ -68,108 +159,20 @@ function displayJoypad(joypad)
   gui.drawText(240, 115, "R")
 end
 
-function display(blocks)
-  for i = 0, 16 do
-    for j = 0, 13 do
-      if (blocks[i][j].value == 1) then
-        gui.drawBox(i * 2 * BoxRadius - BoxRadius, j * 2 * BoxRadius - BoxRadius + 32, i * 2 * BoxRadius + BoxRadius, j * 2 * BoxRadius + BoxRadius + 32, "white") 
-      end
-      if (blocks[i][j].value == 2) then
-        gui.drawBox(i * 2 * BoxRadius - BoxRadius, j * 2 * BoxRadius - BoxRadius + 32, i * 2 * BoxRadius + BoxRadius, j * 2 * BoxRadius + BoxRadius + 32, "red")
-      end
-    end
-  end
-end
-
-function sigmoid(x)
-  return (1 / (1 + e^(-x)))
-end
-
-function firstLayer(specie)
-  local blocks = specie.blocks
-  
-  for i = 0, 16 do
-    for j = 0, 13 do
-      if (blocks[i][j].value ~= nil) then print("Wowzers! " .. i .. " " .. j) 
-        blocks[i][j].sig = sigmoid(blocks[i][j].value * blocks[i][j].weightA + blocks[i][j].weightB)
-      end
-    end
-  end
-  
-  return specie
-end
-
-function secondLayer(specie)
-  local nodes = specie.buttons
-  for i = 0, 6 do
-    local previous = nodes.previous
-    
-    local average = 0
-    for j, block in ipairs(previous) do
-      average = average + specie.blocks[block[0]][block[1]].sig
-    end
-    average = average / #previous
-    
-    if (average > 0.5) then nodes[i].pressed = true else nodes[i].pressed = false end
-  end
-  
-  return specie
-end
-
-function sendInput(input)
-  local buttons
-  buttons["P1 A"] = input[0].pressed
-  buttons["P1 B"] = input[1].pressed
-  buttons["P1 Down"] = input[2].pressed
-  buttons["P1 Left"] = input[3].pressed
-  buttons["P1 Right"] = input[4].pressed
-  buttons["P1 Up"] = input[5].pressed
-  
-  buttons["P1 Select"] = false
-  buttons["P1 Start"] = false
-  
-  joypad.set(buttons)
-end
-
-function getRandomSpecie() 
-  local specie = {}
-  local blocks = {}
-  specie.buttons = {}
-  specie.buttons.previous = {}
-  
-  for i = 0, 16 do
-    blocks[i] = {}
-    for j = 0, 13 do
-      local block = {}
-      block.weightA = math.random()
-      block.weightB = math.random()
-      blocks[i][j] = blocks
-      specie.buttons.previous[math.floor(math.random(6))][#specie.buttons.previous[math.floor(math.random(6))]][0] = i;
-      specie.buttons.previous[math.floor(math.random(6))][#specie.buttons.previous[math.floor(math.random(6))]][1] = j;
-    end
-  end
-  
-  specie.blocks = blocks
-  return specie
-end
-
-  
-local specie = getRandomSpecie()
+local genome = getRandomGenome(1)
   
 while true do
   memory.writebyte(0x075A, 2)
-  memory.writebyte(0x0787, 0x02)
+  --memory.writebyte(0x0787, 0x02)
   getPlayerLocation()
   
   local blocks = {}
   blocks = getBlocks(blocks)
   blocks = getEnemies(blocks)
+  testSpecie(genome[0], blocks)
+  
   display(blocks)
   displayJoypad(joypad.getimmediate())
-  
-  specie = firstLayer(specie)
-  specie = secondLayer(specie)
-  sendInput(specie.buttons)
   
   gui.text(0, 10, "Player Position: " .. PlayerX .. " " .. PlayerY)
   emu.frameadvance()
